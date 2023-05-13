dc3afd593a9d7c144a253608659f008a6e272508
timo13113 <timger2357@gmail.com>
2023-05-13 12:34:10 +0300

    init

---
+++ bot.py
@@ -0,0 +1,37 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+import psycopg2
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="EJM Бот для Хакатона 2023")
+    
+
+conn = psycopg2.connect(
+   database="postgres", user='postgres', password='ndohA6xh8Gzl4i4GSf2g', host='containers-us-west-179.railway.app', port= '7396'
+)
+#Creating a cursor object using the cursor() method
+cursor = conn.cursor()
+
+#Executing an MYSQL function using the execute() method
+cursor.execute("select version()")
+
+# Fetch a single row using fetchone() method.
+data = cursor.fetchone()
+print("Connection established to: ",data)
+
+#Closing the connection
+conn.close()
+
+
+
+
+app = ApplicationBuilder().token("6141821836:AAGJHF9TS2wsPCemhVirF5QqjXs-ezGlKhY").build()
+
+app.add_handler(CommandHandler("hello", hello))
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
